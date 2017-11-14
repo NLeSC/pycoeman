3dada6a2d60e44718b3bffdc919a701d5dcd475b
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2017-11-14 14:05:45 +0100

    Remove function which is not needed.

--- pycoeman/utils_execution.py
+++ pycoeman/utils_execution.py
@@ -2,29 +2,6 @@
 import os
 from pycoeman.monitor import monitor_cpu_mem_disk
 import os,subprocess
-from lxml import etree
-
-def readGCPXMLFile(xmlFile):
-    gcpsXYZ = {}
-    cpsXYZ = {}
-
-    if not os.path.isfile(xmlFile):
-        raise Exception('ERROR: ' + xmlFile + ' not found')
-
-    e = etree.parse(xmlFile).getroot()
-    for p in e.getchildren():
-        gcp = p.find('NamePt').text
-        fields = p.find('Pt').text.split()
-        incertitude = p.find('Incertitude').text
-
-        x = float(fields[0])
-        y = float(fields[1])
-        z = float(fields[2])
-        if incertitude.count('-1'):
-            cpsXYZ[gcp] = (x, y, z)
-        else:
-            gcpsXYZ[gcp] = (x, y, z)
-    return (gcpsXYZ, cpsXYZ)
 
 def getSize(absPath):
     (out,err) = subprocess.Popen('du -sb ' + absPath, shell = True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
